be66f80ce603996eac4f4e375d6c9e6ef19a9626
kandarp dangi <kandarpdangi@gmail.com>
2021-07-19 12:03:24 +0530

    adding services

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { Todo } from './../model/Todo';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  todos: Todo[] = [];
+  constructor() {
+    this.todos = [
+      {
+        id: '101',
+        title: 'Learn Javascript',
+        isComplete: true,
+        date: new Date(),
+      },
+      {
+        id: '102',
+        title: 'Learn Typescript',
+        isComplete: true,
+        date: new Date(),
+      },
+      {
+        id: '103',
+        title: 'Learn Angular',
+        isComplete: false,
+        date: new Date(),
+      },
+    ];
+  }
 
-  constructor() { }
+  getTodos() {
+    return of(this.todos);
+  }
+
+  addTodo(todo: Todo) {
+    this.todos.push(todo);
+  }
+
+  changeTodo(todo: Todo) {
+    this.todos.map((oneTodo) => {
+      if (oneTodo.id === todo.id) {
+        todo.isComplete = !todo.isComplete;
+      }
+    });
+  }
+
+  deleteTodo(todo: Todo) {
+    const indexOfTodo = this.todos.findIndex(
+      (currentObject) => currentObject.id === todo.id
+    );
+    this.todos.splice(indexOfTodo, 1);
+  }
 }
